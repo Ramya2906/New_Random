a1c7423d60b359be990bb0deb3d4d49bd3220764
vijayrao110125 <vijayrao110125@gmail.com>
2022-06-09 10:31:21 +0530

    commit

--- app.py
+++ app.py
@@ -2,10 +2,8 @@ from flask import Flask, render_template, request
 import pickle
 import numpy as np
 
-model = pickle.load(open('iri.pkl', 'rb'))
-
 app = Flask(__name__)
-
+model = pickle.load(open('iri.pkl', 'rb'))
 
 
 @app.route('/')
